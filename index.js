67487d5264529c098b24dd6f0ec042411944918b
Coder Himanshu <83962514+cnshu@users.noreply.github.com>
2021-10-09 20:04:49 +0530

    you are using unwanted nested `req.on`
    The nested `req.on` is causing the application to fall in a loop please solve this error.

--- index.js
+++ index.js
@@ -6,47 +6,44 @@ var biguint = require('biguint-format');
 const crypto = require('crypto');
 var config = require('./config/config.global');
 app.set('view engine', 'pug');
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile('views/payment.html', { root: __dirname })
 });
 
-app.post('/start/payment', function (req, res) {
+console.log(config);
+app.post('/start/payment', function(req, res) {
     let body = '';
     req.on('data', chunk => {
         body += chunk.toString(); // convert Buffer to string
     });
     req.on('end', () => {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString(); // convert Buffer to string
-        });
-        req.on('end', () => {
-            let result = parse(body);
-            console.log(result);
-            let obj = new Payment(config.MERCHANT_ID, config.ACCESS_TOKEN, config.ENC_KEY, config.IS_LIVE);
-            obj.initOrder("ORD_" + biguint(random(8), 'dec'), result.productName, result.amount,
-                'http://localhost:3000/paykun/success', 'http://localhost:3000/paykun/fail', 'INR');
-            obj.addCustomer(result.customerName, result.customerEmail, result.customerMobile);
-            obj.setCustomFields({'udf_1': 'some dummy data'});
-            let requestData = obj.submit();
-            res.render(__dirname + '/views/request', {
-                action:requestData.action,
-                encrypted_request: requestData.encrypted_request,
-                merchant_id: requestData.merchant_id,
-                access_token: requestData.access_token,
-            });
+        let result = parse(body);
+        let obj = new Payment(config.MERCHANT_ID, config.ACCESS_TOKEN, config.ENC_KEY, config.IS_LIVE);
+        obj.initOrder("ORD_" + biguint(random(8), 'dec'), result.productName, result.amount,
+            'http://localhost:8080/paykun/success', 'http://localhost:8080/paykun/fail', 'INR');
+        obj.addCustomer(result.customerName, result.customerEmail, result.customerMobile);
+        obj.setCustomFields({ 'udf_1': 'useful data' });
+        let requestData = obj.submit();
+        res.render(__dirname + '/views/request', {
+            action: requestData.action,
+            encrypted_request: requestData.encrypted_request,
+            merchant_id: requestData.merchant_id,
+            access_token: requestData.access_token,
         });
     });
-
 });
 
-app.get('/paykun/success', function (req, res) {
+
+
+
+
+app.get('/paykun/success', function(req, res) {
     let queryParam = req.query;
     console.log(123123);
     let obj = new Payment(config.MERCHANT_ID, config.ACCESS_TOKEN, config.ENC_KEY, config.IS_LIVE);
     obj.getStatus(queryParam['payment-id'],
-        function(transactionDetail){
-            if(transactionDetail.data.transaction.status == true) {
+        function(transactionDetail) {
+            if (transactionDetail.data.transaction.status == true) {
                 res.render(__dirname + '/views/success', {
                     transactionId: transactionDetail.data.transaction.payment_id,
                     status: transactionDetail.data.transaction.status,
@@ -64,11 +61,11 @@ app.get('/paykun/success', function (req, res) {
         }
     );
 });
-app.get('/paykun/fail', function (req, res) {
+app.get('/paykun/fail', function(req, res) {
     let queryParam = req.query;
     let obj = new Payment(config.MERCHANT_ID, config.ACCESS_TOKEN, config.ENC_KEY, config.IS_LIVE);
     obj.getStatus(queryParam['payment-id'],
-        function(transactionDetail){
+        function(transactionDetail) {
             res.render(__dirname + '/views/fail', {
                 transactionId: transactionDetail.data.transaction.payment_id,
                 status: transactionDetail.data.transaction.status,
@@ -79,16 +76,16 @@ app.get('/paykun/fail', function (req, res) {
     );
 });
 
-app.post('/process/webhook', express.json({type: '*/*'}), (req, res) => {
+app.post('/process/webhook', express.json({ type: '*/*' }), (req, res) => {
     let webhookData = req.body;
-    if(webhookData.hasOwnProperty("transaction") && webhookData.transaction != null) {
+    if (webhookData.hasOwnProperty("transaction") && webhookData.transaction != null) {
         let obj = new Payment(config.MERCHANT_ID, config.ACCESS_TOKEN, config.ENC_KEY, config.IS_LIVE);
-        if(obj.compareSignature(webhookData.transaction, webhookData.transaction.signature)) {
+        if (obj.compareSignature(webhookData.transaction, webhookData.transaction.signature)) {
             //Signature matched
             console.log("Signature matched");
             //now check status and status flag property to verify transaction is success or not
 
-            if(webhookData.transaction.status === 'Success' && webhookData.transaction.status_flag == 1) {
+            if (webhookData.transaction.status === 'Success' && webhookData.transaction.status_flag == 1) {
                 //Transaction is success
                 console.log("Transaction is success");
             } else {
@@ -108,4 +105,4 @@ app.post('/process/webhook', express.json({type: '*/*'}), (req, res) => {
 function random(qty) {
     return crypto.randomBytes(qty)
 }
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on http://localhost:8080'))
